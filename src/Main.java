b0b0033172e4caad9e83597e1a78aaf2f1c8be67
Martina Jireckova <martina.jireckova@gmail.com>
2019-02-28 18:44:08 +0000

    work on stuff

--- src/Main.java
+++ src/Main.java
@@ -18,27 +18,41 @@ public class Main {
             numOfPics = Integer.parseInt(line.substring(0,1));
             myPhotos = new ArrayList<Photo>();
             line = br.readLine();
+
             while(line!= null){
                 String[] words = line.split(" ");
                 int numOfTags = Integer.parseInt(words[1]);
                 String[] tags = new String[numOfTags];
 
                 int j = 0;
-                for(int i = 2; i < numOfTags;i++){
+                for(int i = 2; i < numOfTags+2;i++){
                     tags[j] = words[i];
                     j++;
+                }
+
+                if(words[0].equals("H")){
+                    horizontalPics++;
+                }
+                if(words[0].equals("V")){
+                    verticalPics++;
                 }
 
                 myPhotos.add(new Photo(words[0],tags));
 
                 line = br.readLine();
             }
+
+
         }
         catch(Exception e){
             System.out.println("File not found");
         }
 
 
+    }
+
+    public void groupTags(){
+        
     }
 
     public static void main(String[] args){
@@ -48,5 +62,12 @@ public class Main {
 
         processFile(new File("src/a_example.txt"));
         System.out.println(myPhotos.size());
+        for(Photo p : myPhotos){
+            String[] tags = p.getTags();
+            for(int i = 0; i < tags.length; i++){
+                System.out.print(tags[i]+ " ");
+            }
+            System.out.println();
+        }
     }
 }
