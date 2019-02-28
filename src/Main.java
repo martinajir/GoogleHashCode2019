134a5b890604d977a18bcdfcb562d8c63bc677e6
Martina Jireckova <martina.jireckova@gmail.com>
2019-02-28 18:05:09 +0000

    nothing

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.io.File;
+
 public class Main {
 
-
-    public  processFile()
+    public String[] processFile(File source){
+        
+    }
 
     public static void main(String[] args){
 
